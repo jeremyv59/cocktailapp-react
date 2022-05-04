da3b5cb0066abdb3d4c5d94a98a7c8aec896d0e2
jérémy vaneste <jeremy.vaneste@free.fr>
2022-05-04 12:10:47 +0200

    implement react router in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import { AppContext } from "./context/AppContext";
+import About from "./pages/About";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <React.Fragment>
       <AppContext.Provider value={null}>
-        <Header></Header>
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<Homepage></Homepage>}></Route>
+            <Route path="/about" element={<About></About>}></Route>
+          </Routes>
+        </BrowserRouter>
       </AppContext.Provider>
     </React.Fragment>
   );
