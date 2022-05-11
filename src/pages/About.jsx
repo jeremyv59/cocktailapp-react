a18cc167a28aeca91eb0767df89bcfb26df24cc2
jérémy vaneste <jeremy.vaneste@free.fr>
2022-05-11 17:12:08 +0200

    about page => ok

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "../pages/about.css";
+import {
+  LinkedinOutlined,
+  InstagramOutlined,
+  FacebookOutlined,
+} from "@ant-design/icons";
 
 const About = () => {
   return (
-    <div>
-      <p>About</p>
+    <div className="container about_container">
+      <h2 className="contact_title">Contactez-nous !</h2>
+      <p className="contact_item">Par Email : jeremy.vaneste@free.fr</p>
+      <p className="contact_item">Par Téléphone : 06-95-77-54-63</p>
+      <div className="socials_container">
+        <LinkedinOutlined className="social_item"></LinkedinOutlined>
+        <InstagramOutlined className="social_item"></InstagramOutlined>
+        <FacebookOutlined className="social_item"></FacebookOutlined>
+      </div>
     </div>
   );
 };
